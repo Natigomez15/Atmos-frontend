8f11ef03ea406c444a9b5bd6f2f7ed93180c2853
Natalie <Natigomez1509@gmail.com>
2026-05-17 21:37:27 -0500

    🧹 Proyecto inicializado y limpio

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1> Proyecto ATMOS</h1>
+      <p>Panel de control en construcción...</p>
+    </div>
+  )
+}
+
+export default App
